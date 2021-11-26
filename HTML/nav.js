046aadac8fc8e5321c1355630d6a25246d8cbdd7
DC <dhruv.chauhan2711@gmail.com>
2021-11-26 21:52:03 +0530

    tsak homepage

---
+++ HTML/nav.js
@@ -0,0 +1,24 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("logo").style.height = "54px";
+    document.getElementById("logo").style.width = "73px";
+    document.getElementById("scroll").style.backgroundColor =
+      "rgba(82,82,82,0.8)";
+    document.getElementById("navtog").style.marginTop = "13px";
+    document.getElementById("scroll").style.alignItems = "flex-start";
+    document.getElementById("navbarNav").style.paddingTop = "0.7rem";
+    document.getElementsByClassName("top-nav")[0].style.display = "inline";
+  } else {
+    document.getElementById("logo").style.height = "130px";
+    document.getElementById("logo").style.width = "175px";
+    document.getElementById("scroll").style.backgroundColor = "transparent";
+    document.getElementById("navtog").style.marginTop = "30px";
+    document.getElementById("scroll").style.alignItems = "flex-start";
+    document.getElementById("navbarNav").style.paddingTop = "35px";
+    document.getElementsByClassName("top-nav")[0].style.display = "none";
+  }
+}
